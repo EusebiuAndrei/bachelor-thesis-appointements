9e9ce18a4dd4629c2498355a387d95455110e79c
Gitea <gitea@fake.local>
2022-01-26 17:17:10 +0200

    use domain exceptions everywhere needed

--- src/modules/appointments/domain/exceptions/index.ts
+++ src/modules/appointments/domain/exceptions/index.ts
@@ -1,22 +1,24 @@
-export class AppointmentNotProposedByStudentException extends Error {
+import DomainException from '../../../shared/domain/DomainException';
+
+export class AppointmentNotProposedByStudentException extends DomainException {
   constructor() {
     super('An appointment should only be proposed by a student');
   }
 }
 
-export class AppointmentNotSentToProfessorException extends Error {
+export class AppointmentNotSentToProfessorException extends DomainException {
   constructor() {
     super('An appointment should only be sent to a professor');
   }
 }
 
-export class AppointmentDurationExceededException extends Error {
+export class AppointmentDurationExceededException extends DomainException {
   constructor() {
     super(`An appointment shouldn't be longer than 45 minutes`);
   }
 }
 
-export class NotAppointmentParticipantException extends Error {
+export class NotAppointmentParticipantException extends DomainException {
   constructor() {
     super(`An appointment can be canceled only by a participant`);
   }
